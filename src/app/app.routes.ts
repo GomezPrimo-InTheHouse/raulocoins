1b68fc4dea7289177026cfee4889d11c6f848065
julian <julian1995ag@gmail.com>
2025-04-19 20:48:14 -0300

    firstt commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'auth/login',
+        pathMatch: 'full' // Redirecciona automáticamente al login
+      },
+      {
+        path: 'auth',
+        loadChildren: () => import('./auth/auth.routes').then(m => m.AUTH_ROUTES)
+      },
+      {
+        path: 'dashboard',
+        loadChildren: () => import('./pages/pages.routes').then(m => m.PAGES_ROUTES)
+      },
+      {
+        path: '**',
+        redirectTo: '/login'  // Si hay una ruta no válida, redirige al login
+      }
+];
