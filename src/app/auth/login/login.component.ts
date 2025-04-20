0f7f41956fc691d0c304bb01dab32fca25e3a811
julian <julian1995ag@gmail.com>
2025-04-20 20:53:52 -0300

    avances en login y register

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
-  imports: [],
+  // No es necesaria la importacion de FormBuilder, FormGroup Ni Validators, en el decorador @Component, ya que se importan en el constructor
+  // y se utilizan en el template del componente.
+  imports: [ReactiveFormsModule, CommonModule],
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  // Creo el FORMGROPUP para el formulario de login
+  // Se importa el ReactiveFormsModule y se inyecta el FormBuilder en el constructor
+  LoginForm: FormGroup;
 
+  constructor( private fb: FormBuilder) {
+    this.LoginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit() {
+    console.log(this.LoginForm.value);
+    if(this.LoginForm.valid) {
+      console.log(this.LoginForm.value);
+      
+    }
+  }
+
+  show:boolean = false;
+
+  showPassword() {
+    this.show = !this.show;
+  }
 }
