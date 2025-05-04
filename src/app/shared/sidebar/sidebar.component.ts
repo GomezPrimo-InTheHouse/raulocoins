3a2710b6f4da59ebbcedc8cd029ccf3f81c3590e
GomezPrimo-InTheHouse <juliangomezprimo@gmail.com>
2025-05-04 12:35:01 -0300

    avances en el sidebar

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { NgClass, NgFor } from '@angular/common';
+
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, NgClass, NgFor],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
+  menuItems = [
+    { label: 'Dashboard', link: '/home/dashboard' },
+    { label: 'Profile', link: '/home/profile' },
+    { label: 'Settings', link: '/home/transactions' },
+  ]
   constructor(private router: Router) {}
 
   
